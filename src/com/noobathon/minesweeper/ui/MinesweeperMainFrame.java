aa27bcbd4e6ff480565f2c09142b860b5e6544a0
Dan Sabin <dan.sabin@gmail.com>
2012-07-15 13:30:10 -0700

    Added menu bars

--- src/com/noobathon/minesweeper/ui/MinesweeperMainFrame.java
+++ src/com/noobathon/minesweeper/ui/MinesweeperMainFrame.java
@@ -2,26 +2,38 @@ package com.noobathon.minesweeper.ui;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
 
-public class MinesweeperMainFrame extends JFrame 
+public class MinesweeperMainFrame extends JFrame implements ActionListener
 {
 	private static final long serialVersionUID = -5099377961864547691L;
 	private final static int DEFAULT_GRID_ROWS = 15;
 	private final static int DEFAULT_GRID_COLUMNS = 15;
+	private MinesweeperGridFrame theGrid;
+	private JMenuBar theMenuBar;
+	private JMenu fileMenu;
 	
 	public MinesweeperMainFrame()
 	{
 		super("Minesweeper");
 		setLayout(new BorderLayout());
+		setupMenu();
 		setupGrid();
 		setupFrame();
 	}
 	
 	private void setupGrid()
 	{
-		this.add(new MinesweeperGridFrame(DEFAULT_GRID_ROWS, DEFAULT_GRID_COLUMNS), BorderLayout.CENTER);
+		theGrid = new MinesweeperGridFrame(DEFAULT_GRID_ROWS, DEFAULT_GRID_COLUMNS);
+		this.add(theGrid, BorderLayout.CENTER);
 	}
 	
 	private void setupFrame()
@@ -31,4 +43,31 @@ public class MinesweeperMainFrame extends JFrame
 		this.setMinimumSize(new Dimension(400, 400));
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
+	
+	private void setupMenu()
+	{
+		theMenuBar = new JMenuBar();
+		fileMenu = new JMenu("File");
+		theMenuBar.add(fileMenu);
+		
+		JMenuItem newGame = new JMenuItem("New Game");
+		newGame.addActionListener(this);
+		
+		newGame.setAccelerator(KeyStroke.getKeyStroke(
+		        KeyEvent.VK_1, ActionEvent.ALT_MASK));
+		newGame.getAccessibleContext().setAccessibleDescription(
+		        "Start a new game");
+		
+		fileMenu.add(newGame);
+		
+		this.setJMenuBar(theMenuBar);
+	}
+	
+	public void actionPerformed(ActionEvent e) 
+	{
+		this.remove(theGrid);
+		theGrid = new MinesweeperGridFrame(DEFAULT_GRID_ROWS, DEFAULT_GRID_COLUMNS);
+		this.add(theGrid);
+		this.pack();
+    }
 }
